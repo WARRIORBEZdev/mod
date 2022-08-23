8031305180631ff85a400af3fd9d835493f18485
WARRIORBEZdev <111262470+WARRIORBEZdev@users.noreply.github.com>
2022-08-23 09:55:38 +0500

    Add files via upload

---
+++ еуые.py
@@ -0,0 +1,51 @@
+
+
+ 
+
+from .. import loader, utils
+from asyncio import sleep
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LegendsMod(loader.Module):
+     
+    
+    strings = {"name": "legends"}
+
+    async def legendscmd(self, message):
+        " BY @warriorbezdev "
+
+    
+        await message.edit("<b> Привет</b>")
+        await sleep(1)
+        await message.edit("как вы")
+        await sleep(0.5)
+        await message.edit("KANAYKO")
+        await sleep(1)
+        await message.edit("WARRIOR")
+        await sleep(1)
+        await message.edit("PUSTOTA")
+        await sleep(1)        
+        await message.edit("GPRV")
+        await sleep(1)
+        await message.edit("AYZL")
+        await sleep(1)
+        await message.edit("LUNATEEEEEC")
+        await sleep(1)
+        await message.edit("этот мод")
+        await sleep(1)
+        await message.edit("посвящается")
+        await sleep(1)
+        await message.edit("всем")
+        await sleep(1)
+        await message.edit("Легендам ")
+        await sleep(1)
+        await message.edit("этого")
+        await sleep(1)
+        await message.edit("чата")
+        await sleep(1)
+        await message.edit("Legends sila")
+        await sleep(1)
+        return
